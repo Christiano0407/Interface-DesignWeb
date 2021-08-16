6bc1fb0d9f8a039ed41741445472adcaa0933aa8
Christiano0407 <placebo0704@gmail.com>
2021-08-16 15:05:10 -0500

    play-Two

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
 console.group(`Clone-Web`); 
 console.log("Menú-lateral"); 
+// < ================ Variables =============== > 
+const nav = document.getElementById(`nav`); 
+const navLeft = document.querySelector(`.nav__left`); 
+const navRight = document.querySelector(`.nav__right`); 
+const navLink = Array.from(document.querySelectorAll(`.nav-link`)); 
+console.log(navLink); 
+const menuToggle = document.getElementById(`menu-toggle`); 
+console.log(menuToggle); 
 
 
+
+// ================= Functions / EVENTS ============> 
+menuToggle.addEventListener(`click`, () => {
+    // Menú / hamburguer ========== >
+    menuToggle.classList.toggle(`active`);
+
+}); 
 
 
 console.groupEnd(); 
