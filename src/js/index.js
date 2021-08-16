748f6ce404bb9d064057d7901913c5d449a7cf68
Christiano0407 <placebo0704@gmail.com>
2021-08-16 15:37:26 -0500

    playerOne

--- src/js/index.js
+++ src/js/index.js
@@ -8,15 +8,31 @@ const navLink = Array.from(document.querySelectorAll(`.nav-link`));
 console.log(navLink); 
 const menuToggle = document.getElementById(`menu-toggle`); 
 console.log(menuToggle); 
-
-
+const contact = document.querySelector(`.contact`); 
 
 // ================= Functions / EVENTS ============> 
+// >>>>>> MenuToggle ==> 
 menuToggle.addEventListener(`click`, () => {
     // Menú / hamburguer ========== >
     menuToggle.classList.toggle(`active`);
 
+    // ========= Nav ====> 
+    nav.classList.toggle(`active`); 
+    // ========= navLeft ==== > 
+    navLeft.classList.toggle(`toggle`); 
+    // ========== NavRight ====== > 
+    navRight.classList.toggle(`active`);
+
+
+    // ========== Remove ================= > 
+     contact.classList.remove(`active`); 
+     navRight.classList.remove(`active`); 
+
+
+
 }); 
+
+// >>> == Conditionals ==>
 
 
 console.groupEnd(); 
