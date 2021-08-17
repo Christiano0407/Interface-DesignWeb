6796c17b6b679166b155fce36ed6ec1445da1789
Christiano0407 <placebo0704@gmail.com>
2021-08-17 09:09:45 -0500

    menu-Btn-js

--- src/js/appMovies.js
+++ src/js/appMovies.js
@@ -1,9 +1,21 @@
 console.group("Movies"); 
 // < ===================== Variables ======================= >
-
-
+const menuBtn = document.querySelector(`.menu-btn`); 
+console.log(menuBtn); 
+let menuOpen = false; 
 
 // < ================= Functions and Events ==============>  
+menuBtn.addEventListener(`click`, () => {
+
+    if(!menuOpen) {
+        menuBtn.classList.add(`open`); 
+        menuOpen = true; 
+    } else {
+        menuBtn.classList.remove(`open`); 
+        menuOpen = false; 
+    }
+
+}); 
 
 
 console.groupEnd(); 
