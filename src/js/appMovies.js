bf838d46edc6c67455cf227bdacaaaf9008ab04f
Christiano0407 <placebo0704@gmail.com>
2021-08-18 10:11:51 -0500

    part-one

--- src/js/appMovies.js
+++ src/js/appMovies.js
@@ -4,15 +4,63 @@ const menuBtn = document.querySelector(`.menu-btn`);
 console.log(menuBtn); 
 let menuOpen = false; 
 
+let nav = document.getElementById(`nav`); 
+let navLeft = document.querySelector(`.nav__left`); 
+let navRight = document.querySelector(`.nav__right`); 
+let contact = document.querySelector(`.contact`); 
+
+let navLink = Array.from(document.querySelectorAll(`.nav-link`)); 
+console.log(navLink); 
+
 // < ================= Functions and Events ==============>  
 menuBtn.addEventListener(`click`, () => {
+    // Active Menu Hamburger > 
+    console.log("burger"); 
 
     if(!menuOpen) {
         menuBtn.classList.add(`open`); 
         menuOpen = true; 
+
+        setTimeout(() => {
+        navRight.classList.add(`active`); 
+        }, 200); 
+
+        setTimeout(() => {
+        contact.classList.add(`active`);
+        }, 400); 
+
     } else {
         menuBtn.classList.remove(`open`); 
         menuOpen = false; 
+
+         contact.classList.remove(`active`);
+    }
+    // < ========= Llamar a navegadores laterales  =========== > 
+    // ClassList and Toggle == > 
+
+    nav.classList.toggle(`active`); 
+
+    navLeft.classList.toggle(`active`); 
+
+    navRight.classList.toggle(`active`); 
+
+  /*   contact.classList.toggle(`active`);  */
+
+    // Remove ClassList === > 
+   
+    navRight.classList.remove(`active`);
+
+    // < =========== Conditionals =========== >
+
+    if(menuBtn.classList.contains(`open`)) {
+         menuOpen = true
+        for(let i = 0; i < navLink.length; i++) {
+            navLink[i].classList.remove(`active`); 
+
+            setTimeout(() => {
+                navLink[i].classList.add(`active`); 
+            }, i * 100); 
+        }
     }
 
 }); 
