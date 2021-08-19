434fb92683446f541568a7ce037855fe8791832f
Christiano0407 <placebo0704@gmail.com>
2021-08-19 12:48:14 -0500

    carousel03

--- src/js/appMovies.js
+++ src/js/appMovies.js
@@ -61,21 +61,76 @@ menuBtn.addEventListener(`click`, () => {
    
     navRight.classList.remove(`active`);
 
-    // < =========== Conditionals =========== >
+});
 
-    /* if() { */
-    /*     */
-    /*       */
-    /*     */
-    /*     */
+ // < =========================== Carousel =========================== >
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
-    /* } */
+ let images = [...document.querySelectorAll(`.img`)]; 
+console.log(images); 
+let slider = document.querySelector(`.slider`); 
+let innerSlider = document.querySelector(`.slider__inner`)
+let pressed = false;
+// Presionada / do 
+let X; 
 
+// < ====================== Events / Functions =============== >
+
+// <<<<< ====== Obtener Imágenes ===== >>>>>
+images.forEach((img, idx) => {
+    img.style.backgroundImage = `url(../assets/image/${idx+1}.jpg)`; 
+});
+// =============== Slider y manita =============== >
+
+// =============== Slider y manita =============== >
+slider.addEventListener(`mousedown`, (e) => {
+   pressed = true; 
+   startX = e.offsetX - innerSlider.offsetLeft; 
+   //console.log(innerSlider.offsetLeft); 
+   slider.style.cursor = "grabbing"; 
 }); 
 
+slider.addEventListener(`mouseenter`, () => {
+    slider.style.cursor = "grab"; 
+}); 
+
+/* slider.addEventListener(`mouseleave`, () => { */
+/*     slider.style.cursor = "default";  */
+/* }); */
+
+slider.addEventListener(`mouseup`, () => {
+    slider.style.cursor = "grab";  
+}); 
+slider.addEventListener(`mouseup`, () => {
+    pressed = false; 
+}); 
+
+// == Ejecutar Scroll == 
+
+// === Ejecutar el scroll === >
+slider.addEventListener(`mousemove`, (e) => {
+     if(!pressed)return; 
+     e.preventDefault(); 
+
+     x = e.offsetX; 
+
+     innerSlider.style.left = `${x - startX}px`; 
+
+     checkbounddary(); 
+}); 
+
+// ===== Scroll con parámetros un stop a las imágenes =========================== > 
+
+checkbounddary = () => {
+  let outer = slider.getBoundingClientRect(); 
+  let inner = innerSlider.getBoundingClientRect(); 
+ 
+  if(parseInt(innerSlider.style.left) > 0) {
+      innerSlider.style.left = `0px`; 
+  }else if(inner.right < outer.right) {
+    innerSlider.style.left = ` -${inner.width - outer.width}px`; 
+  }
+}; 
+
+//checkbounddary(); 
 
 console.groupEnd(); 
